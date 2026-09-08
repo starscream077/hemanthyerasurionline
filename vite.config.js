c6cfd49f8d5d37a7b8f184fda26d993f4ae84fe6
Hemanth Yerasuri <hemanthyerasuri@gmail.com>
2026-09-08 17:03:41 +0530

    Compile Tailwind styles with Vite

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // Replace 'repo-name' with whatever you name your GitHub repository
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: './',
 })
