fc32deef4262b24923f66e8cfbeefb17174aebab
Hemanth Yerasuri <hemanthyerasuri@gmail.com>
2026-09-08 16:59:50 +0530

    Fix asset paths for custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Replace 'repo-name' with whatever you name your GitHub repository
 export default defineConfig({
   plugins: [react()],
-  base: '/hemanthyerasurionline/', 
+  base: './',
 })
